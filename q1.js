0b2ab5332cfed85501df7f42cc9c70e18fef2d9a
eAreebk <156355720+eAreebk@users.noreply.github.com>
2024-01-20 22:15:58 -0800

    Add files via upload
    Basic Data types example

---
+++ q1.js
@@ -0,0 +1,12 @@
+/*write simple and basic javascript program to Declare two variables named age and isStudent. 
+Assign your agetothe age variable and specify whether you are a student or not usingthe isStudent variable. 
+Print the values of both variables to the console.*/
+
+var age = 21;
+var isStudent = false;
+
+console.log("   Asg1 Q1");
+console.log("Your Age is ", age);
+console.log("Are you isStudent?", isStudent);
+
+
---
+++ q2.js
@@ -0,0 +1,9 @@
+/*Create a program that uses the alert method to display a welcomemessage to the user when they open the webpage. 
+The messageshould include a greeting and a brief introduction. */
+
+
+var greet = "You're Welcome to Our Website";
+var intro = 'I am Muhammad Areeb, learing web develpment at initial level. Thanks for visiting here.  ';
+var wmsg = greet + "\n" + intro;
+
+alert(wmsg);
